c9967ced4248e6e39c977c9b2ad18673d4603064
Anne Spurlock <anne.spurlock16@gmail.com>
2022-10-02 18:59:58 -0700

    fetched data from API

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,37 @@
 let map;
-let sanfranciscoLatLong = { lat: 37.773972, lng: -122.431297 }; 
-const sanjoseLatLong = { lat: 37.338207, lng: -121.886330 };
+let centerUSLatLong = { lat: 38.500000, lng: -98.000000 }; 
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: sanfranciscoLatLong,
-    zoom: 8,
+    center: centerUSLatLong,
+    zoom: 4,
     mapId: "354e515ae59101d6",
   });
 }
 
-function addMarkerToSanJose() {
-    const marker = new google.maps.Marker({
-        position: sanjoseLatLong, 
-        label: "San Jose",
-        map: map,
-    });
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b9e94fe346mshe89ab685882efa5p1248bcjsndee919de9077',
+		'X-RapidAPI-Host': 'covid-19-statistics.p.rapidapi.com'
+	}
+};
+
+const RAPID_API_HOST = 'https://covid-19-statistics.p.rapidapi.com';
+const provinceEndpoint = `${RAPID_API_HOST}/provinces?iso=USA`
+
+async function getCovidLocations() {
+  const response = await fetch(provinceEndpoint, options);
+  const {data} = await response.json();
+  return data;
 }
+
+async function getReportsByProvince(province) {
+  const reportsEndpoint = `${RAPID_API_HOST}/reports?region_province=${province}`
+
+  const response = await fetch(reportsEndpoint, options);
+  const {data} = await response.json();
+  return data;
+}
+
+
